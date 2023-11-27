42e5083371576fd3e1ac3fcc798886ba00a49b8e
Shayla Luong <91114896@mvusd.net>
2023-11-27 13:23:03 -0800

    Initial commit

---
+++ src/Actor.java
@@ -0,0 +1,5 @@
+//maybe i can use this to create the player, enemies, etc
+public class Actor {
+    private String name;
+    private String HP;
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+        //preparation
+        Scanner scan = new Scanner (System.in);
+        boolean playing = true;
+        String inputStr;
+        int inputInt; //twooo types of input ig
+
+        //welcome + tutorial text
+        System.out.println("Welcome to BLAH BLAH BLAH SOMETHING\n");
+        System.out.println("HERES HOW TO PLAY OR SOMETHING\n");
+
+        //loop for playing game?
+        while(playing)
+        {
+            inputStr = scan.nextLine();
+            inputInt = scan.nextLine();
+        }
+    }
+}
