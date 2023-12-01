1386017e1e206e282ce063bdb3fa81e188a8dad8
shayla <69056399+Blaizenheart@users.noreply.github.com>
2023-12-01 14:23:47 -0800

    Update Actor.java

--- src/Actor.java
+++ src/Actor.java
@@ -1,7 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.Math;
-import java.util.Random;
 
 public class Actor
 {
@@ -9,19 +8,19 @@ public class Actor
     private String name; // the actor's name
     private int level; // the actor's level
     private int xp; // the amount of xp accumulated
-    
+
     private int atk; // the actor's attack stat
     private int hp; // amount of hit points the actor currently has
     private int maxHp; // the maximum amount of hit points the actor can have
     private int def; // the actor's defense stat
-    
+
     private String weapon; // the weapon the actor has equipped
     private String playerClass; // the player's class (mercenary/knight/darkPriest/outlander)
     private int gold; // the amount of gold pieces the actor has
-    
-    private List<String> inventory; // the actor's posessions
+
+    private List<String> inventory; // the actor's possessions
     private List<String> spellsList; // the spells the actor knows
-    
+
     private boolean deathState; // true -> dead, false -> alive
 
     ////////////////////////////// CONSTRUCTORS //////////////////////////////
@@ -29,7 +28,7 @@ public class Actor
     public Actor()
     {
         name = "";
-        level = 0;
+        level = 1;
         xp = 0;
         atk = 0;
         hp = 100;
@@ -42,7 +41,7 @@ public class Actor
         spellsList = new ArrayList<>();
         deathState = false;
     }
-    
+
     //Full Constructor (used for the player only)
     public Actor(String name, int level, int xp, int atk, int hp, int maxHp, int def, String weapon, String playerClass, int gold, List<String> inventory, List<String> spellsList, boolean deathState)
     {
@@ -80,57 +79,77 @@ public class Actor
     {
         return name;
     }
-    
+
     public int getLevel()
     {
         return level;
     }
-    
+
     public int getXP()
     {
         return xp;
     }
-    
+
     public int getAtk()
     {
         return atk;
     }
-    
+
     public int getHp()
     {
         return hp;
     }
-    
+
     public int getMaxHp()
     {
         return maxHp;
     }
-    
+
     public int getDef()
     {
         return def;
     }
-    
+
     public String getWeapon()
     {
         return weapon;
     }
-    
+
     public String getPlayerClass()
     {
         return playerClass;
     }
-    
+
     public int getGold()
     {
         return gold;
     }
-    
+
     public boolean getDeathState()
     {
         return deathState;
     }
-    
+
+    public boolean hasSpell(String spellName) // checks to see if the player has a certain spell
+    {
+        return spellsList.contains(spellName);
+    }
+
+    public boolean hasItem(String itemName)
+    {
+        return inventory.contains(itemName);
+    }
+
+    public int inventoryIndex(String itemName)
+    { // returns the index of a certain item in the actor's inventory
+        return inventory.indexOf(itemName);
+    }
+
+    public int inventorySize()
+    {
+        return inventory.size();
+    }
+
     ////////////////////////////// SETTERS //////////////////////////////
     public void setName(String name)
     {
@@ -156,12 +175,12 @@ public class Actor
     {
         this.maxHp = maxHp;
     }
-    
+
     public void setDef(int def)
     {
         this.def = def;
     }
-    
+
     public void setWeapon(String weapon)
     {
         this.weapon = weapon;
@@ -176,17 +195,22 @@ public class Actor
     {
         this.gold = gold;
     }
-    
-    public void addItem(String item) // adds an item to the actor's inventory
+
+    public void addItem(String itemName) // adds an item to the actor's inventory
     {
-        inventory.add(item);
+        inventory.add(itemName);
     }
-    
+
+    public void subItem(int index) // adds an item to the actor's inventory
+    {
+        inventory.remove(index);
+    }
+
     public void addSpell(String spell) // adds a spell to the actor's spells list
     {
         spellsList.add(spell);
     }
-    
+
     public void addHp(int hpAdd) // adds health
     {
         if ((hp + hpAdd) > maxHp) // checks to see if the health added would exceed max hp
@@ -198,7 +222,7 @@ public class Actor
             hp += hpAdd;
         }
     }
-    
+
     public void subHp(int hpSub)
     {
         if ((hp - hpSub) < 0) // checks to see if the health subtracted would be below 0
@@ -210,24 +234,19 @@ public class Actor
             hp -= hpSub;
         }
     }
-    
+
     public void addGold(int goldAdd)
     {
         gold += goldAdd;
     }
-    
+
     public void subGold(int goldSub)
     {
         gold -= goldSub;
     }
-    
+
     ////////////////////////////// BRAIN METHODS //////////////////////////////
-    
-    public boolean hasSpell(String spellName) // checks to see if the player has a certain spell
-    {
-        return spellsList.contains(spellName);
-    }
-    
+
     public boolean isDead() // checks to see if the actor is dead
     {
         boolean result = false;
@@ -238,7 +257,7 @@ public class Actor
         }
         return result;
     }
-    
+
     public int dmgCalc() // calculates the amount of damage done
     {
         int damage;
@@ -259,16 +278,20 @@ public class Actor
         {
             additionalAtk = 2;
         }
+        else if (weapon.equals("cleaver"))
+        {
+            additionalAtk = 4;
+        }
         else
         {
             additionalAtk = 0;
         }
         damage = atk + additionalAtk;
-        
+
         // arbitrary damage calculations
-        damage *= 10; 
+        damage *= 10;
         damage /= 2;
-        
+
         // variation in damage
         damage += Game.generator.nextInt(41)-20;
         if (damage <= 0)
@@ -277,7 +300,7 @@ public class Actor
         }
         return damage;
     }
-    
+
     public int spellDmgCalc(String spell) // calculates the damage of a spell
     {
         int damage = (int) (atk * level * 0.7);
@@ -295,7 +318,7 @@ public class Actor
             damage *= 10;
             damage /= 2;
         }
-        
+
         // variation in damage
         damage += Game.generator.nextInt(41)-20;
         if (damage <= 0)
@@ -304,7 +327,7 @@ public class Actor
         }
         return damage;
     }
-    
+
     public boolean missChance() //calculates if the actor misses an attack
     {
         boolean result = false;
@@ -315,7 +338,7 @@ public class Actor
         }
         return result;
     }
-    
+
     public boolean enemyMissChance(String playerClass) // calculates if the enemy misses an attack
     {
         boolean result = false;
@@ -336,7 +359,7 @@ public class Actor
         }
         return result;
     }
-    
+
     public boolean runChance() // calculates whether the player is successful in running away
     {
         boolean result = false;
@@ -347,16 +370,31 @@ public class Actor
         }
         return result;
     }
-    
+
+    public void levelCalc(int xpGained)
+    {
+        xp += xpGained;
+        level += (xp/100);
+        xp = xp % 100;
+    }
     ////////////////////////////// TO STRING //////////////////////////////
     public String openInventory() // displays the player's inventory
     {
-        String output = "\n-----INVENTORY-----\n";
-        for (String item : inventory)
+        String output = "";
+        if (inventory.size() > 0)
         {
-            output += "☆ " + item + "\n";
+            output += "\n-----INVENTORY-----\n";
+            for (String item : inventory)
+            {
+                output += "☆ " + item + "\n";
+            }
+        }
+        else
+        {
+            output += "You don't have anything in your inventory.";
         }
         return output;
+
     }
 
     public String openSpells() // displays the list of spells the player knows
@@ -373,12 +411,15 @@ public class Actor
     {
         String output = "\n-----PROFILE-----\n";
         output += "Name: " + name;
-        output += "\nLevel : " + level;
+        output += "\nLevel: " + level;
+        output += "\nXP: " + xp + "/100";
+        output += xpVisual();
         output += "\nClass: " + playerClass.toUpperCase();
         output += "\nEquipped: " + weapon;
         output += "\nGold: " + gold;
         output += "\n-----STATS-----";
-        output += "\nHP: " + hp;
+        output += "\nHP: " + hp + "/100";
+        output += hpVisual();
         output += "\nATK: " + atk;
         output += "\nDEF: " + def;
         output += "\n-----INVENTORY-----\n";
@@ -393,12 +434,12 @@ public class Actor
         }
         return output;
     }
-    
+
     public String hpVisual() // creates a visual for the actor's hp bar
     {
         String output = "\n[";
         // calculates the percentage the actor's hp is at
-        double percent = ((double) hp / maxHp) * 100; 
+        double percent = ((double) hp / maxHp) * 100;
         int count = (int) Math.round(percent / 10); // reduces the size of the hp visual by 10
         for (int i = 0; i < count; i++)
         {
@@ -410,6 +451,26 @@ public class Actor
         }
         output += "]";
         output += "\n" + hp + "/" + maxHp;
+        output += "\n";
+        return output;
+    }
+
+    public String xpVisual() // creates a visual for the actor's hp bar
+    {
+        String output = "\n[";
+        // calculates the percentage the actor's hp is at
+        double percent = ((double) xp / 100) * 100;
+        int count = (int) Math.round(percent / 10); // reduces the size of the hp visual by 10
+        for (int i = 0; i < count; i++)
+        {
+            output += "X";
+        }
+        for (int j = 0; j < 10 - count; j++)
+        {
+            output += "-";
+        }
+        output += "]";
+        output += "\n" + xp + "/" + 100;
         output += "\n";
         return output;
     }
